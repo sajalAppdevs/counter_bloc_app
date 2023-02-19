93cb27364d555088b0bd9b019f32c34142878793
jakansha2001 <jakansha2001@gmail.com>
2023-02-20 04:20:21 +0530

    built basic screens

---
+++ lib/home/screens/home_page.dart
@@ -0,0 +1,11 @@
+import 'package:counter_bloc_app/home/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const HomeScreen();
+  }
+}
