04a35d635d5844776a8350b7f2c43d8e0c621d28
jakansha2001 <jakansha2001@gmail.com>
2023-02-20 04:27:16 +0530

    built basic home page of counter app

--- lib/home/screens/home_screen.dart
+++ lib/home/screens/home_screen.dart
@@ -5,6 +5,24 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(
+          Icons.add,
+        ),
+      ),
+      appBar: AppBar(
+        title: const Text(
+          'Counter App',
+        ),
+      ),
+      body: const Center(
+        child: Text(
+          '0',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:counter_bloc_app/home/screens/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -9,14 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      home: HomePage()
     );
   }
 }
